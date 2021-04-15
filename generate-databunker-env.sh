424f894087c3a3f17a2110d42b979e1c5ff3644b
Yuli <stremovsky@gmail.com>
2021-04-15 22:00:44 +0300

    fix for mac

--- generate-databunker-env.sh
+++ generate-databunker-env.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-KEY=`< /dev/urandom tr -dc 'a-f0-9' | head -c${1:-48};`
+KEY=`< /dev/urandom LC_CTYPE=C tr -dc 'a-f0-9' | head -c${1:-48};`
 echo 'DATABUNKER_MASTERKEY='$KEY > databunker.env
-
